680afafd806f14a6fbed1419f096c90501afbe41
sergetoure <serge.toure@eviatech.com>
2026-09-10 17:45:56 +0200

    remove decimals

--- script.js
+++ script.js
@@ -22,7 +22,11 @@ myForm.addEventListener("submit", (event) => {
   targetAvgPricePerTitre = parseFloat(formData.get("targetAvgPricePerTitre"));
   actualPriceTitreToBuy = parseFloat(formData.get("actualPriceTitreToBuy"));
   const result = calculatorTitre(actualNbreTitreEnPortefeuille, avgPricePerTitreEnProtefeuille, targetAvgPricePerTitre, actualPriceTitreToBuy);
-  const formatter = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' });
+  const formatter = new Intl.NumberFormat('fr-FR', { 
+    style: 'currency', 
+    currency: 'EUR', 
+    maximumFractionDigits: 0 // Removes decimals
+  });
   document.getElementById("result").innerHTML = result>= 0 ? `Nombre de titres à acheter: <span>${Math.round(result)}</span> titres au prix de <span>${formatter.format(Math.floor(actualPriceTitreToBuy))}</span> pour un montant total de <span>${formatter.format(Math.floor(Math.round(result) * actualPriceTitreToBuy * fraisCourtage))}</span> vous permettra d'atteindre un prix moyen de <span>${formatter.format(targetAvgPricePerTitre)}</span> par titre.`: `Le prix moyen cible est déjà atteint ou dépassé. Aucun achat nécessaire.`;
 
 });
